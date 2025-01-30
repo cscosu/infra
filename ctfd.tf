6b9f3edd91a76108d0510bbfb5307d76e43b0cf0
Mark Bundschuh <mark@mbund.dev>
2025-01-30 01:50:33 -0500

    ctfd

--- ctfd.tf
+++ ctfd.tf
@@ -14,11 +14,43 @@ resource "aws_instance" "ctfd" {
     }
   }
 
-  user_data = base64encode("#!/bin/bash\n\necho \"ECS_CLUSTER=${aws_ecs_cluster.default.name}\" > /etc/ecs/ecs.config\n")
+  user_data = base64encode(<<-INIT
+    #!/bin/bash
+    
+    echo "ECS_CLUSTER=${aws_ecs_cluster.default.name}" > /etc/ecs/ecs.config
+
+    cat <<EOF > /etc/systemd/system/mount-ebs.service
+    [Unit]
+    Description=Format and Mount Device
+    DefaultDependencies=no
+    Before=local-fs.target
+    Wants=local-fs.target
+
+    [Service]
+    Type=oneshot
+    ExecStartPre=/bin/bash -c '(while ! /usr/bin/lsblk -ln -o FSTYPE /dev/sdh 2>/dev/null; do echo "Waiting for block device /dev/sdh..."; sleep 2; done); sleep 2'
+    ExecStart=/bin/bash -c "if [ x`/usr/bin/lsblk -ln -o FSTYPE /dev/sdh` != 'ext4' ] ; then /usr/sbin/mkfs.ext4 -L ctfd /dev/sdh ; fi && /usr/bin/mkdir -p /ctfd && /usr/bin/mount /dev/sdh /ctfd"
+    ExecStop=/usr/bin/umount /ctfd
+    RemainAfterExit=yes
+
+    [Install]
+    WantedBy=multi-user.target
+    EOF
+
+    systemctl daemon-reload
+    systemctl enable mount-ebs
+    systemctl start mount-ebs
+
+    INIT
+  )
 
   tags = {
     Name = "${local.name}-ec2-ctfd"
   }
+}
+
+resource "random_password" "ctfd_jwt_secret_key" {
+  length = 64
 }
 
 resource "aws_ecs_task_definition" "ctfd" {
@@ -27,56 +59,228 @@ resource "aws_ecs_task_definition" "ctfd" {
   container_definitions = jsonencode([
     {
       name              = "ctfd"
-      image             = "traefik/whoami:latest"
+      image             = "ghcr.io/ctfd/ctfd:3.7.5"
       memoryReservation = 200
+
+      links = ["redis", "mariadb"],
+
+      dependsOn = [
+        {
+          containerName = "redis",
+          condition     = "HEALTHY"
+        },
+        {
+          containerName = "mariadb",
+          condition     = "HEALTHY"
+        },
+      ]
+
+      mountPoints = [
+        {
+          sourceVolume  = "ctfd"
+          containerPath = "/ctfd"
+          readOnly      = false
+        }
+      ]
+
+      entryPoint = ["/bin/sh", "-c", "python3 -c \"import urllib.request; import zipfile; open('plugin.zip', 'wb').write(urllib.request.urlopen('https://github.com/cscosu/ctfd-writeups/archive/refs/heads/main.zip').read()); zipfile.ZipFile('plugin.zip', 'r').extractall('plugin')\" && cp -r plugin/ctfd-writeups-main/ctfd-writeups CTFd/plugins/ctfd-writeups && /opt/CTFd/docker-entrypoint.sh"]
+
+      dockerLabels = {
+        "traefik.enable"                                      = "true"
+        "traefik.http.routers.ctfd.rule"                      = "Host(`ctfd.testing.osucyber.club`)"
+        "traefik.http.routers.ctfd.entrypoints"               = "websecure"
+        "traefik.http.routers.ctfd.tls.certResolver"          = "myresolver",
+        "traefik.http.services.ctfd.loadbalancer.server.port" = "32769"
+      }
 
       portMappings = [
         {
-          containerPort = 80
+          containerPort = 8000
           hostPort      = 32769
           name          = "ctfd"
         }
       ]
 
-      dockerLabels = {
-        "traefik.enable"                                        = "true"
-        "traefik.http.routers.whoami.rule"                      = "Host(`whoami.testing.osucyber.club`)"
-        "traefik.http.routers.whoami.entrypoints"               = "websecure"
-        "traefik.http.routers.whoami.tls.certResolver"          = "myresolver",
-        "traefik.http.services.whoami.loadbalancer.server.port" = "32769"
+      environment = [
+        {
+          name  = "SECRET_KEY",
+          value = random_password.ctfd_jwt_secret_key.result,
+        },
+        {
+          name  = "UPLOAD_FOLDER"
+          value = "/ctfd/uploads",
+        },
+        {
+          name  = "LOG_FOLDER"
+          value = "/ctfd/logs",
+        },
+        {
+          name  = "REVERSE_PROXY",
+          value = "true",
+        },
+        {
+          name  = "DATABASE_URL",
+          value = "mysql+pymysql://ctfd:ctfd@mariadb/ctfd",
+        },
+        {
+          name  = "REDIS_URL",
+          value = "redis://redis:6379",
+        }
+      ]
+
+      healthCheck = {
+        retries = 3
+        command = [
+          "CMD-SHELL",
+          "true"
+        ]
+        timeout  = 3
+        interval = 10
       }
 
-      #   mountPoints = [
-      #     {
-      #       sourceVolume  = "redis-data"
-      #       containerPath = "/data"
-      #       readOnly      = false
-      #     }
-      #   ]
-      systemControls = []
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.default.name
+          "awslogs-region"        = local.region,
+          "awslogs-stream-prefix" = "ctfd",
+        }
+      }
 
-      #   healthCheck = {
-      #     retries = 3
-      #     command = [
-      #       "CMD-SHELL",
-      #       "redis-cli ping"
-      #     ]
-      #     timeout  = 3
-      #     interval = 10
-      #   }
+      stopTimeout = 300
+    },
+    {
+      name              = "redis"
+      image             = "redis:7.4.2"
+      memoryReservation = 200
+
+      mountPoints = [
+        {
+          sourceVolume  = "redis"
+          containerPath = "/data"
+          readOnly      = false
+        }
+      ]
+
+      healthCheck = {
+        retries = 3
+        command = [
+          "CMD-SHELL",
+          "redis-cli ping"
+        ]
+        timeout  = 3
+        interval = 10
+      }
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.default.name
+          "awslogs-region"        = local.region,
+          "awslogs-stream-prefix" = "ctfd",
+        }
+      }
+
+      stopTimeout = 300
+    },
+    {
+      name              = "mariadb"
+      image             = "mariadb:11.6.2-ubi"
+      memoryReservation = 200
+
+      command = [
+        "mysqld",
+        "--character-set-server=utf8mb4",
+        "--collation-server=utf8mb4_unicode_ci",
+        "--wait_timeout=28800",
+        "--log-warnings=0",
+      ]
+
+      environment = [
+        {
+          name  = "MARIADB_ROOT_PASSWORD",
+          value = "ctfd",
+        },
+        {
+          name  = "MARIADB_USER",
+          value = "ctfd",
+        },
+        {
+          name  = "MARIADB_PASSWORD",
+          value = "ctfd",
+        },
+        {
+          name  = "MARIADB_DATABASE",
+          value = "ctfd",
+        },
+        {
+          name  = "MARIADB_AUTO_UPGRADE",
+          value = "1",
+        },
+      ]
+
+      mountPoints = [
+        {
+          sourceVolume  = "mariadb"
+          containerPath = "/var/lib/mysql"
+          readOnly      = false
+        }
+      ]
+
+      healthCheck = {
+        retries = 3
+        command = [
+          "CMD-SHELL",
+          "true"
+        ]
+        timeout  = 3
+        interval = 10
+      }
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.default.name
+          "awslogs-region"        = local.region,
+          "awslogs-stream-prefix" = "ctfd",
+        }
+      }
 
       stopTimeout = 300
     }
   ])
 
-  #   volume {
-  #     name      = "redis-data"
-  #     host_path = "/redis-data"
-  #   }
+  volume {
+    name      = "ctfd"
+    host_path = "/ctfd/ctfd"
+  }
+
+  volume {
+    name      = "redis"
+    host_path = "/ctfd/redis"
+  }
+
+  volume {
+    name      = "mariadb"
+    host_path = "/ctfd/mariadb"
+  }
 
   requires_compatibilities = []
   tags                     = {}
 }
+
+resource "aws_ebs_volume" "ctfd" {
+  availability_zone = aws_instance.ctfd.availability_zone
+  size              = 20
+  type              = "gp3"
+}
+
+resource "aws_volume_attachment" "ctfd" {
+  device_name = "/dev/sdh"
+  instance_id = aws_instance.ctfd.id
+  volume_id   = aws_ebs_volume.ctfd.id
+}
+
 
 resource "aws_ecs_service" "ctfd" {
   depends_on      = [aws_iam_role_policy.ecs_cluster_permissions]
